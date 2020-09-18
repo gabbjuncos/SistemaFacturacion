3acb5319191559bb7a293e8b3aefb40acf63ea42
gjuncos <gabbjuncos@gmail.com>
2020-09-18 20:05:58 +0100

    avance formulario proyecto

---
+++ SistemaFacturacion/SistemaFacturacion/Clases/Proyecto.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace SistemaFacturacion.Clases
+{
+    class Proyecto
+    {
+        //creo objeto dato como un atributo mas de esta clase
+        Datos oDato = new Datos();
+
+        private int id_proyecto;
+        private int id_producto;
+        private int id_responsable;
+        private string descripcion;
+        private string version;
+        private string alcance;
+        private bool borrado;
+
+        public int Id_proyecto { get => id_proyecto; set => id_proyecto = value; }
+        public int Id_producto { get => id_producto; set => id_producto = value; }
+        public int Id_responsable { get => id_responsable; set => id_responsable = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public string Version { get => version; set => version = value; }
+        public string Alcance { get => alcance; set => alcance = value; }
+        public bool Borrado { get => borrado; set => borrado = value; }
+
+        public bool validarDatosProyecto()
+        {
+            //validamos preguntando por todo lo que estaria mal cargado a los atributos del objeto Proyecto
+            if (this.descripcion.ToString() == string.Empty)
+            {
+                MessageBox.Show("El campo DESCRIPCION esta vacio");
+                return false;
+
+            }
+            if (this.version == string.Empty)
+            {
+                MessageBox.Show("El campo VERSION esta vacio");
+                return false;
+
+            }
+            if (this.alcance == string.Empty)
+            {
+                MessageBox.Show("El campo ALCANCE esta vacio");
+                return false;
+
+            }
+
+           
+            if (this.id_producto.ToString() == string.Empty | this.id_producto < 0)
+            {
+                MessageBox.Show("El campo ID PRODUCTO esta vacio o no es un valor valido");
+                return false;
+
+            }
+
+            if (this.id_responsable.ToString() == string.Empty | this.id_responsable < 0)
+            {
+                MessageBox.Show("El campo ID RESPONSABLE esta vacio o no es un valor valido");
+                return false;
+
+            }
+
+
+            return true;
+        }
+
+        // metodo para verificar si existe el cliente con un determinado cuit y devuelve si o no
+        public bool existe()
+
+        {
+            DataTable table = new DataTable();
+            table = oDato.consultar("SELECT * FROM Proyectos WHERE id_proyecto= '" + this.id_producto + "'");
+            if (table.Rows.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        //metodo para realizar la consulta sql referia al insert de los datos que se quieren grabar 
+        public void grabarProyecto()
+        {
+            string sqlInsert = "INSERT INTO Proyectos (id_producto, descripcion, version, alcance, id_responsable, borrado)" +
+                " VALUES ( " +
+                this.id_producto + ", '" +
+                this.descripcion + "','" +
+                this.version + "','" +
+                this.alcance + "', " +
+                this.id_responsable + ", " +
+                0 + ")";
+
+            //ejecutamos el metodo para realizar la consulta en la BD pasando la consulta insert
+            oDato.actualizar(sqlInsert);
+
+
+        }
+        //metodo para realizar la consulta sql referia al update de los datos que se quieren grabar 
+        public void actualizarProyecto()
+        {
+            string sqlUpdate = "UPDATE Proyectos SET " +
+                " id_producto = " + this.id_producto + " ," +
+                " descripcion = '" + this.descripcion + "', " +
+                " version = '" + this.version + "', " +
+                " alcance = '" + this.alcance + "', " +
+                " id_responsable = " + this.id_responsable + ", " +
+                " borrado = " + 0 + "," +
+                " WHERE id_proyecto = " + this.id_proyecto;
+
+
+            //ejecutamos el metodo para realizar la consulta en la BD pasando la consulta insert
+            oDato.actualizar(sqlUpdate);
+
+        }
+
+        //metodo para crear data table cargada con los datos de Producto a partir de la consulta, teniendo solo encuenta los no borrados(N) 
+        public DataTable recuperarProyectos()
+        {
+            string consultaSQL = "SELECT pr.id_proyecto, pr.borrado, p.id_producto, pr.descripcion, pr.version, pr.alcance, u.id_usuario" +
+                " FROM Proyectos pr, Productos p, Usuarios u" +
+                " WHERE pr.borrado = 0" +
+                " AND pr.id_responsable = u.id_usuario" +
+                " AND pr.id_producto = p.id_producto";
+
+            return oDato.consultar(consultaSQL);
+        }
+    }
+}
---
+++ SistemaFacturacion/SistemaFacturacion/frmProyecto.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SistemaFacturacion.Clases;
+
+namespace SistemaFacturacion
+{
+    public partial class frmProyecto : Form
+    {
+        //flag para saber cuando se activa boton nuevo o no (boton modificar)
+        bool nuevo = false;
+
+        // creo objeto Proyecto
+        Proyecto oProyecto = new Proyecto();
+
+        public frmProyecto()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmProyecto_Load(object sender, EventArgs e)
+        {
+            //Carga el formulario deshabilitando los campos y habilitando solo botones nuevo, editar, borrar y salir
+            this.habilitar(false);
+
+        }
+
+        //metodo para habilitar o deshabilitar campos y botones
+        private void habilitar(bool x)
+        {
+            txtIdProyecto.Enabled = false; //id proyecto desabilitado por que es autoincremental
+            txtIdProducto.Enabled = x;
+            txtDescripcion.Enabled = x;
+            txtVersion.Enabled = x;
+            txtAlcance.Enabled = x;
+            txtIdResponsable.Enabled = x;
+ 
+            //habilitar botones o deshabilitar
+            btnGrabar.Enabled = x;
+            btnCancelar.Enabled = x;
+            btnNuevo.Enabled = !x;
+            btnEditar.Enabled = !x;
+            btnBorrar.Enabled = !x;
+            btnSalir.Enabled = !x;
+            grdProyecto.Enabled = !x;
+
+        }
+
+        //metodo para limpiar campos 
+        private void limpiar()
+        {
+            txtIdProyecto.Clear();
+            txtIdProducto.Clear();
+            txtIdResponsable.Clear();
+            txtAlcance.Clear();
+            txtDescripcion.Clear();
+            txtVersion.Clear();
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            //activo flag nuevo
+            this.nuevo = true;
+
+            //Al hacer click en Nuevo habilitamos botones grabar y cancelar y tambien los campos       
+            this.habilitar(true);
+            //limpamos campos cajas texto
+            this.limpiar();
+            //hacemos focus en el campo CUIT
+            this.txtVersion.Focus();
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            //tomamos los valores de las cajas de texto y se lo asignamos a un objeto cliente
+            oProyecto.Id_producto = int.Parse(txtIdProducto.Text);
+            oProyecto.Id_responsable = int.Parse(txtIdProducto.Text);
+            oProyecto.Descripcion = txtDescripcion.Text;
+            oProyecto.Version = txtVersion.Text;
+            oProyecto.Alcance = txtAlcance.Text;
+
+            //validamos los datos antes de grabar
+            if (oProyecto.validarDatosProyecto())
+            {
+                //si la bandera dice que es nuevo hacemos agregamos los datos
+                if (this.nuevo)
+                {
+                    //si el usuario que se quiere insertar no existe entonces lo grabamos
+                    if (!oProyecto.existe())
+                    {
+
+                        oProyecto.grabarProyecto();
+
+                    }
+
+                    //si el usuario que se quiere insertar ya existe
+                    else
+                    {
+                        MessageBox.Show("El Proyecto ya existente");
+                    }
+                }
+                //si la bandera dice que NO es nuevo se trata de una modficacion , entonces actualizaciomos datos
+                else
+                {
+                    oProyecto.Id_proyecto = int.Parse(txtIdProyecto.Text);
+                    oProyecto.actualizarProyecto();
+                }
+
+                //actualizamos grilla
+                cargarGrilla(grdProyecto, oProyecto.recuperarProyectos());
+                MessageBox.Show("El PROYECTOS se grabo correctamente");
+
+            }
+
+
+            //click en grabar deshabilitando los campos y habilitando solo botones nuevo, editar, borrar y salir
+            this.habilitar(false);
+            //desabilito la bandera nuevo
+            this.nuevo = false;
+        }
+
+        // metodo para cargar la grilla com su respectivos campos tomando como parametro la grilla y databla para hacerlos coincidir, 
+        private void cargarGrilla(DataGridView grilla, DataTable tabla)
+        {
+            //limpiamos grilla 
+            grilla.Rows.Clear();
+
+
+            //recorremos el datable, contamos filas para limite superior del for
+            for (int i = 0; i < tabla.Rows.Count; i++)
+            {
+
+
+                //cargamos la fila de la gilla, en el orden que ponemos cada instruccion es el orden de la columna de la grilla a la que se va a cargar el correspondiente valor de la columna que se indica con el nombre en el databla
+                grilla.Rows.Add(tabla.Rows[i]["id_proyecto"],
+                                tabla.Rows[i]["borrado"],
+                                tabla.Rows[i]["id_producto"],
+                                tabla.Rows[i]["descripcion"],
+                                tabla.Rows[i]["version"],
+                                tabla.Rows[i]["alcance"],
+                                tabla.Rows[i]["id_responsable"]);
+
+
+            }
+
+        }
+    }
+}
