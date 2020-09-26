825dd707f22d94878a65aeb50f5fe358befabdc8
gjuncos <gabbjuncos@gmail.com>
2020-09-26 18:54:37 +0100

    avance transaccion Facturacion

--- SistemaFacturacion/SistemaFacturacion/frmFacturas.Designer.cs
+++ SistemaFacturacion/SistemaFacturacion/frmFacturas.Designer.cs
@@ -28,12 +28,404 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.grdFacturaDetalle = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnQuitar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtPrecioTotal = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.cboProducto = new System.Windows.Forms.ComboBox();
+            this.cboProyecto = new System.Windows.Forms.ComboBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.lblProyecto = new System.Windows.Forms.Label();
+            this.lblNroFactura = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.txtNroFactura = new System.Windows.Forms.TextBox();
+            this.cboCliente = new System.Windows.Forms.ComboBox();
+            this.cboUsuario = new System.Windows.Forms.ComboBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.id_factura = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nroDeOrden = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.proyecto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdFactura = new System.Windows.Forms.TextBox();
+            this.lblNroOrden = new System.Windows.Forms.Label();
+            this.txtNroOrden = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grdFacturaDetalle)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(118, 32);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.TabIndex = 0;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(62, 35);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(37, 13);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // grdFacturaDetalle
+            // 
+            this.grdFacturaDetalle.AllowUserToAddRows = false;
+            this.grdFacturaDetalle.AllowUserToDeleteRows = false;
+            this.grdFacturaDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdFacturaDetalle.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_factura,
+            this.nroDeOrden,
+            this.producto,
+            this.proyecto,
+            this.precio});
+            this.grdFacturaDetalle.Location = new System.Drawing.Point(105, 101);
+            this.grdFacturaDetalle.Name = "grdFacturaDetalle";
+            this.grdFacturaDetalle.ReadOnly = true;
+            this.grdFacturaDetalle.Size = new System.Drawing.Size(603, 150);
+            this.grdFacturaDetalle.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNroOrden);
+            this.groupBox1.Controls.Add(this.lblNroOrden);
+            this.groupBox1.Controls.Add(this.btnQuitar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.lblTotal);
+            this.groupBox1.Controls.Add(this.txtPrecioTotal);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.cboProducto);
+            this.groupBox1.Controls.Add(this.cboProyecto);
+            this.groupBox1.Controls.Add(this.lblPrecio);
+            this.groupBox1.Controls.Add(this.lblProducto);
+            this.groupBox1.Controls.Add(this.lblProyecto);
+            this.groupBox1.Controls.Add(this.grdFacturaDetalle);
+            this.groupBox1.Location = new System.Drawing.Point(32, 144);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(756, 303);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnQuitar
+            // 
+            this.btnQuitar.Location = new System.Drawing.Point(633, 63);
+            this.btnQuitar.Name = "btnQuitar";
+            this.btnQuitar.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitar.TabIndex = 12;
+            this.btnQuitar.Text = "Quitar";
+            this.btnQuitar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(633, 24);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 11;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(531, 280);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total";
+            // 
+            // txtPrecioTotal
+            // 
+            this.txtPrecioTotal.Location = new System.Drawing.Point(582, 277);
+            this.txtPrecioTotal.Name = "txtPrecioTotal";
+            this.txtPrecioTotal.Size = new System.Drawing.Size(91, 20);
+            this.txtPrecioTotal.TabIndex = 9;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(493, 27);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 8;
+            // 
+            // cboProducto
+            // 
+            this.cboProducto.FormattingEnabled = true;
+            this.cboProducto.Location = new System.Drawing.Point(304, 26);
+            this.cboProducto.Name = "cboProducto";
+            this.cboProducto.Size = new System.Drawing.Size(121, 21);
+            this.cboProducto.TabIndex = 7;
+            // 
+            // cboProyecto
+            // 
+            this.cboProyecto.FormattingEnabled = true;
+            this.cboProyecto.Location = new System.Drawing.Point(109, 26);
+            this.cboProyecto.Name = "cboProyecto";
+            this.cboProyecto.Size = new System.Drawing.Size(121, 21);
+            this.cboProyecto.TabIndex = 6;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(450, 30);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 5;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(248, 30);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(50, 13);
+            this.lblProducto.TabIndex = 4;
+            this.lblProducto.Text = "Producto";
+            // 
+            // lblProyecto
+            // 
+            this.lblProyecto.AutoSize = true;
+            this.lblProyecto.Location = new System.Drawing.Point(44, 30);
+            this.lblProyecto.Name = "lblProyecto";
+            this.lblProyecto.Size = new System.Drawing.Size(49, 13);
+            this.lblProyecto.TabIndex = 3;
+            this.lblProyecto.Text = "Proyecto";
+            // 
+            // lblNroFactura
+            // 
+            this.lblNroFactura.AutoSize = true;
+            this.lblNroFactura.Location = new System.Drawing.Point(368, 36);
+            this.lblNroFactura.Name = "lblNroFactura";
+            this.lblNroFactura.Size = new System.Drawing.Size(66, 13);
+            this.lblNroFactura.TabIndex = 4;
+            this.lblNroFactura.Text = "Nro. Factura";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(386, 78);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(39, 13);
+            this.lblCliente.TabIndex = 5;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(65, 78);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 6;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // txtNroFactura
+            // 
+            this.txtNroFactura.Location = new System.Drawing.Point(440, 32);
+            this.txtNroFactura.Name = "txtNroFactura";
+            this.txtNroFactura.Size = new System.Drawing.Size(121, 20);
+            this.txtNroFactura.TabIndex = 7;
+            // 
+            // cboCliente
+            // 
+            this.cboCliente.FormattingEnabled = true;
+            this.cboCliente.Location = new System.Drawing.Point(440, 78);
+            this.cboCliente.Name = "cboCliente";
+            this.cboCliente.Size = new System.Drawing.Size(121, 21);
+            this.cboCliente.TabIndex = 8;
+            // 
+            // cboUsuario
+            // 
+            this.cboUsuario.FormattingEnabled = true;
+            this.cboUsuario.Location = new System.Drawing.Point(120, 75);
+            this.cboUsuario.Name = "cboUsuario";
+            this.cboUsuario.Size = new System.Drawing.Size(121, 21);
+            this.cboUsuario.TabIndex = 9;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(542, 475);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(371, 475);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Location = new System.Drawing.Point(255, 474);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(75, 23);
+            this.btnGrabar.TabIndex = 12;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(129, 474);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 13;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // id_factura
+            // 
+            this.id_factura.HeaderText = "ID Factura";
+            this.id_factura.Name = "id_factura";
+            this.id_factura.ReadOnly = true;
+            // 
+            // nroDeOrden
+            // 
+            this.nroDeOrden.HeaderText = "Nro. Orden";
+            this.nroDeOrden.Name = "nroDeOrden";
+            this.nroDeOrden.ReadOnly = true;
+            this.nroDeOrden.Width = 110;
+            // 
+            // producto
+            // 
+            this.producto.HeaderText = "Producto";
+            this.producto.Name = "producto";
+            this.producto.ReadOnly = true;
+            this.producto.Width = 110;
+            // 
+            // proyecto
+            // 
+            this.proyecto.HeaderText = "Proyecto";
+            this.proyecto.Name = "proyecto";
+            this.proyecto.ReadOnly = true;
+            this.proyecto.Width = 110;
+            // 
+            // precio
+            // 
+            this.precio.HeaderText = "Precio";
+            this.precio.Name = "precio";
+            this.precio.ReadOnly = true;
+            this.precio.Width = 110;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(589, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "ID Factura";
+            // 
+            // txtIdFactura
+            // 
+            this.txtIdFactura.Location = new System.Drawing.Point(665, 34);
+            this.txtIdFactura.Name = "txtIdFactura";
+            this.txtIdFactura.Size = new System.Drawing.Size(100, 20);
+            this.txtIdFactura.TabIndex = 15;
+            // 
+            // lblNroOrden
+            // 
+            this.lblNroOrden.AutoSize = true;
+            this.lblNroOrden.Location = new System.Drawing.Point(44, 68);
+            this.lblNroOrden.Name = "lblNroOrden";
+            this.lblNroOrden.Size = new System.Drawing.Size(59, 13);
+            this.lblNroOrden.TabIndex = 16;
+            this.lblNroOrden.Text = "Nro. Orden";
+            // 
+            // txtNroOrden
+            // 
+            this.txtNroOrden.Location = new System.Drawing.Point(109, 65);
+            this.txtNroOrden.Name = "txtNroOrden";
+            this.txtNroOrden.Size = new System.Drawing.Size(121, 20);
+            this.txtNroOrden.TabIndex = 17;
+            // 
+            // frmFacturas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmFacturas";
+            this.ClientSize = new System.Drawing.Size(800, 540);
+            this.Controls.Add(this.txtIdFactura);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.cboUsuario);
+            this.Controls.Add(this.cboCliente);
+            this.Controls.Add(this.txtNroFactura);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.lblNroFactura);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.dtpFecha);
+            this.Name = "frmFacturas";
+            this.Text = "Factura";
+            this.Load += new System.EventHandler(this.frmFacturas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdFacturaDetalle)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.DataGridView grdFacturaDetalle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtPrecioTotal;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.ComboBox cboProducto;
+        private System.Windows.Forms.ComboBox cboProyecto;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Label lblProyecto;
+        private System.Windows.Forms.Label lblNroFactura;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtNroFactura;
+        private System.Windows.Forms.ComboBox cboCliente;
+        private System.Windows.Forms.ComboBox cboUsuario;
+        private System.Windows.Forms.Button btnQuitar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_factura;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nroDeOrden;
+        private System.Windows.Forms.DataGridViewTextBoxColumn producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn proyecto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdFactura;
+        private System.Windows.Forms.TextBox txtNroOrden;
+        private System.Windows.Forms.Label lblNroOrden;
     }
 }
--- SistemaFacturacion/SistemaFacturacion/frmFacturas.cs
+++ SistemaFacturacion/SistemaFacturacion/frmFacturas.cs
@@ -7,14 +7,180 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SistemaFacturacion.Clases;
 
 namespace SistemaFacturacion
 {
     public partial class frmFacturas : Form
     {
+        Datos oBD = new Datos();
+         
+
         public frmFacturas()
         {
             InitializeComponent();
+        }
+
+        private void frmFacturas_Load(object sender, EventArgs e)
+        {
+            //Carga el formulario deshabilitando los campos y habilitando solo botones nuevo, editar, borrar y salir
+            this.habilitar(false);
+
+            this.cargarCombo(cboCliente, "Clientes", 2);
+            this.cargarCombo(cboProducto, "Productos", 1);
+            this.cargarCombo(cboProyecto, "Proyectos", 2);
+            this.cargarCombo(cboUsuario, "Usuarios", 2);
+        }
+
+        //metodo para habilitar o deshabilitar campos y botones
+        private void habilitar(bool x)
+        {
+            txtNroFactura.Enabled = x;
+            txtPrecio.Enabled = x;
+            dtpFecha.Enabled = x;
+
+            cboCliente.Enabled = x;
+            cboUsuario.Enabled = x;
+            cboProducto.Enabled = x;
+            cboProyecto.Enabled = x;
+
+
+            btnGrabar.Enabled = x;
+            btnCancelar.Enabled = x;
+            btnAgregar.Enabled = x;
+            btnQuitar.Enabled = x;
+            btnNuevo.Enabled = !x;
+
+            btnSalir.Enabled = !x;
+            grdFacturaDetalle.Enabled = !x;
+
+        }
+
+        //metodo para limpiar campos 
+        private void limpiar()
+        {
+            
+            
+            txtNroFactura.Clear();
+            txtPrecio.Clear();
+            
+            //para que apunten siempre al primero los combos por defecto
+            cboCliente.SelectedIndex = 0;
+            cboProducto.SelectedIndex = 0;
+            cboProyecto.SelectedIndex = 0;
+            cboUsuario.SelectedIndex = 0;
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            //Al hacer click en Nuevo habilitamos botones, agregar,quitar, cancelar y tambien los campos       
+            this.habilitar(true);
+            //limpamos campos cajas texto
+            this.limpiar();
+            //hacemos focus en el campo CUIT
+            this.txtNroFactura.Focus();
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+
+            //click en grabar deshabilitando los campos y habilitando solo botones nuevo, borrar y salir
+            this.habilitar(false);
+
+            //ejecutamos metodo validar para verificar que se haya cargado la cabecera de la factura y sus detalles
+            if (validarFactura()) {
+
+                //verificamos que haya detalles cargados
+                if (grdFacturaDetalle.Rows.Count > 0)
+                {
+                    //nos conectamos con trasaccion
+                    DBHelper.GetDBHelper();
+                    //grabo cabezera
+                    grabarFactura();
+                    //grabo detalle
+                    grabarFacturaDetalle();
+                    //y desconectarse de la base de datos a partir aca se hace el commit o el roolback
+                    DBHelper.GetDBHelper.();
+                   
+                }
+
+
+                else {
+                    MessageBox.Show("Debe tener al menos un DETALLE DE FACTURA.");
+
+                }
+            }
+        }
+
+        private bool validarFactura()
+        {   //validamos que la fecha no este vacia y quede marcado con color lo que falta cargar
+            if (dtpFecha.Value == null) {
+                MessageBox.Show("Debe ingresar una FECHA");
+                dtpFecha.CalendarTitleBackColor = Color.Red;
+                dtpFecha.Focus();
+
+                return false;
+            }
+            //verificamos que la caja de texto nro factura no este vacia y quede marcado con color para cargar
+            if (txtNroFactura.Text == string.Empty) {
+                txtNroFactura.BackColor = Color.Red;
+                txtNroFactura.Focus();
+                return false;
+            }
+
+
+            return true;
+        }
+
+
+            private void btnCancelar_Click(object sender, EventArgs e)
+        // click en cancelar deshabilitando los campos y habilitando solo botones nuevo, y salir
+        {
+            this.habilitar(false);
+            
+            
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cargarCombo(ComboBox combo, string nombreTabla, int numeroColumnaDisplay)
+        {
+
+            DataTable tabla = new DataTable();
+
+            tabla = oBD.consultarTabla(nombreTabla);
+            combo.DataSource = tabla;
+            combo.DisplayMember = tabla.Columns[numeroColumnaDisplay].ColumnName;    // para nombre
+            combo.ValueMember = tabla.Columns[0].ColumnName;      //para ide
+            combo.DropDownStyle = ComboBoxStyle.DropDownList;  //por si no lo hago por las propeidades para que no se pueda editar cuando escribo en el combo en ejecucion
+            //combo.SelectedIndex = -1; // queda apuntando a la nada cuando se ejecuta 
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            //agregamos a grilla el detalle colocado
+            grdFacturaDetalle.Rows.Add(txtIdFactura.Text,txtNroOrden.Text,cboProducto.DisplayMember, cboProyecto.DisplayMember,txtPrecio);
+
+            //calculamos precio total de la grilla y lo ponemos en la caja de texto precio total
+            txtPrecioTotal.Text = calcularTotal().ToString();
+        }
+
+
+        //metodo para calcular el precio total de los detalles
+
+        private double calcularTotal() {
+            double total = 0;
+
+            for (int i = 0; i < grdFacturaDetalle.Rows.Count ; i++)
+            {
+                total += Convert.ToDouble(grdFacturaDetalle.Rows[i].Cells[4].Value);
+            }
+
+            return total;
         }
     }
 }
